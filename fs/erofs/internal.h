a1097861c6556bd78f8dc277cdbb94b3485779f8
Gao Xiang <hsiangkao@linux.alibaba.com>
2021-10-09 04:08:37 +0800

    erofs: get compression algorithms directly on mapping [backport]
    Previously, compression algorithm types are got on reading each page.
    Actually, it can be obtained directly when mapping blocks.
    
    In addition, it's also a preparation for the upcoming secondary
    compression algorithms.
    
    Link: https://lore.kernel.org/r/20211010213145.17462-6-xiang@kernel.org
    Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    Signed-off-by: Zears14 <79321017+Zears14@users.noreply.github.com>

--- fs/erofs/internal.h
+++ fs/erofs/internal.h
@@ -325,7 +325,7 @@ extern const struct address_space_operations z_erofs_aops;
  * of the corresponding uncompressed data in the file.
  */
 enum {
-	BH_Zipped = BH_PrivateStart,
+	BH_Encoded = BH_PrivateStart,
 	BH_FullMapped,
 };
 
@@ -333,8 +333,8 @@ enum {
 #define EROFS_MAP_MAPPED	(1 << BH_Mapped)
 /* Located in metadata (could be copied from bd_inode) */
 #define EROFS_MAP_META		(1 << BH_Meta)
-/* The extent has been compressed */
-#define EROFS_MAP_ZIPPED	(1 << BH_Zipped)
+/* The extent is encoded */
+#define EROFS_MAP_ENCODED	(1 << BH_Encoded)
 /* The length of extent is full */
 #define EROFS_MAP_FULL_MAPPED	(1 << BH_FullMapped)
 
@@ -342,6 +342,7 @@ struct erofs_map_blocks {
 	erofs_off_t m_pa, m_la;
 	u64 m_plen, m_llen;
 
+	char m_algorithmformat;
 	unsigned int m_flags;
 
 	struct page *mpage;
@@ -349,6 +350,10 @@ struct erofs_map_blocks {
 
 /* Flags used by erofs_map_blocks_flatmode() */
 #define EROFS_GET_BLOCKS_RAW    0x0001
+enum {
+	Z_EROFS_COMPRESSION_SHIFTED = Z_EROFS_COMPRESSION_MAX,
+	Z_EROFS_COMPRESSION_RUNTIME_MAX
+};
 
 /* zmap.c */
 #ifdef CONFIG_EROFS_FS_ZIP
